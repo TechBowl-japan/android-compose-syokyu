593b87fe1e0682408f64bcfd3a11e7098a0e5730
Tamaki Hidetsugu <git@ralph.ms>
2025-02-06 02:11:35 +0900

    Add files

---
+++ app/src/main/java/com/example/techtrain/railway/android/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.techtrain.railway.android
+
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme {
+                MainScreen()
+            }
+        }
+    }
+}
+
+@Composable
+fun MainScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+    }
+}
+
+@Composable
+@Preview
+fun MainScreenPreview() {
+    MaterialTheme {
+        MainScreen()
+    }
+}
